add1a83f630edb9714af85d7be55f8e6b24e63eb
Perlmanaya1 <ayala57194@gmail.com>
2022-12-01 23:36:38 +0200

    changq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftSideComponent } from './components/left-side/left-side.component';
+import { RightSideComponent } from './components/right-side/right-side.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideComponent,
+    RightSideComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
